7eb2677da814172fe153f834f7d3e0cb3825932e
Yotam Medini <yotam.medini@gmail.com>
2025-01-20 20:44:13 +0200

    SynthSequencer construction done

--- synthseq.cpp
+++ synthseq.cpp
@@ -1,6 +1,37 @@
 #include "synthseq.h"
+#include <fmt/core.h>
+#include <fluidsynth.h>
+// #include <fluidsynth/settings.h>
 
-SynthSequencer::SynthSequencer() {
+SynthSequencer::SynthSequencer(const std::string &sound_font_path) {
+  settings_ = new_fluid_settings();
+  int fs_rc;
+  if (ok()) {
+    fs_rc = fluid_settings_setint(settings_, "synth.reverb.active", 0);
+    if (fs_rc != FLUID_OK) {
+      error_ = fmt::format("setting reverb: failed rc={}", fs_rc);
+    }
+  }
+  if (ok()) {
+    fs_rc = fluid_settings_setint(settings_, "synth.chorus.active", 0);
+    if (fs_rc != FLUID_OK) {
+      error_ = fmt::format("setting chorus: failed rc={}", fs_rc);
+    }
+  }
+  if (ok()) {
+    synth_ = new_fluid_synth(settings_);
+    sfont_id_ = fluid_synth_sfload(synth_, sound_font_path.c_str(), 1);
+    if (sfont_id_ == FLUID_FAILED) {
+      error_ = fmt::format("failed: sfload({})", sound_font_path);
+    }
+  }
+  if (ok()) {
+    audio_driver_ = new_fluid_audio_driver(settings_, synth_);
+  }
+  if (ok()) {
+    sequencer_ = new_fluid_sequencer2(0);
+    synth_seq_id_ = fluid_sequencer_register_fluidsynth(sequencer_, synth_);
+  }
 }
 
 SynthSequencer::~SynthSequencer() {
--- synthseq.h
+++ synthseq.h
@@ -2,15 +2,21 @@
 #pragma once
 
 #include <cstdint>
+#include <string>
 #include <fluidsynth/types.h>
 
 class SynthSequencer {
  public:
-  SynthSequencer();
+  SynthSequencer(const std::string &sound_font_path);
   ~SynthSequencer();
-  fluid_synth_t *settings_{nullptr};
+  bool ok() const { return error_.empty(); }
+  const std::string &error() const { return error_; }
+  fluid_settings_t *settings_{nullptr};
   fluid_synth_t *synth_{nullptr};
   fluid_audio_driver_t *audio_driver_{nullptr};
+  fluid_sequencer_t *sequencer_{nullptr};
   int16_t synth_seq_id_{-1};  
-  int16_t sfont_id{-1};
+  int16_t sfont_id_{-1};
+ private:
+  std::string error_;
 };
