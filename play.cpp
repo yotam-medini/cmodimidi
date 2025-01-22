3c214301a1b56060d7d757f74d482704ffdcb127
Yotam Medini <yotam.medini@gmail.com>
2025-01-22 18:46:50 +0200

    SetIndexEvents

--- play.cpp
+++ play.cpp
@@ -1,20 +1,69 @@
 #include "play.h"
+#include <algorithm>
 #include <iostream>
+#include <numeric>
+#include <tuple>
+#include <vector>
+#include <fmt/core.h>
 #include "synthseq.h"
 
+class IndexEvent {
+ public:
+  IndexEvent(uint32_t time=0, size_t track=0, size_t tei=0) :
+    time_{time}, track_{track}, tei_{tei} {}
+  uint32_t time_{0}; // sum of delta_time
+  size_t track_{0};
+  size_t tei_{0}; // track event index;
+};
+bool operator<(const IndexEvent& ie0, const IndexEvent& ie1) {
+  return
+    std::tie(ie0.time_, ie0.track_, ie0.tei_) <
+    std::tie(ie1.time_, ie1.track_, ie1.tei_);
+}
+  
 class Player {
  public:
   Player(const midi::Midi &pm, SynthSequencer &ss, const PlayParams &pp) :
     pm_{pm}, ss_{ss}, pp_{pp} {}
   int run();
+
+ private:
+  void SetIndexEvents();
+  void SetAbsEvents();
   int rc_{0};
+
   const midi::Midi &pm_; // parsed_midi
   SynthSequencer &ss_;
   const PlayParams &pp_;
+
+  std::vector<IndexEvent> index_events_;
 };
+
+void Player::SetIndexEvents() {
+  const std::vector<midi::Track> &tracks = pm_.GetTracks();
+  const size_t ne = std::accumulate(tracks.begin(), tracks.end(), size_t{0},
+    [](size_t r, const midi::Track &track) {
+      return r + track.events_.size();
+    });
+  if (pp_.debug_ & 0x1) { std::cerr << fmt::format("Total events: {}\n", ne); }
+  index_events_.reserve(ne);
+  for (size_t ti = 0; ti < tracks.size(); ++ti) {
+    const midi::Track &track = tracks[ti];
+    const std::vector<std::unique_ptr<midi::Event>> &events = track.events_;
+    const size_t nte = events.size();
+    uint32_t time = 0;
+    for (size_t tei = 0; tei < nte; ++tei) {
+      uint32_t dt = events[tei]->delta_time_;
+      time += dt;
+      index_events_.push_back(IndexEvent(time, ti, tei));
+    }
+  }
+  std::sort(index_events_.begin(), index_events_.end());
+}
 
 int Player::run() {
   if (pp_.debug_ & 0x1) { std::cerr << "Player::run() begin\n"; }
+  SetIndexEvents();
   if (pp_.debug_ & 0x1) { std::cerr << "Player::run() end\n"; }
   return rc_;
 }
