a99e240800c3cd1230021bf0726f2330913ee10e
Yotam Medini <yotam.medini@gmail.com>
2025-04-03 18:18:12 +0300

    modidump2ly: program options with required

--- tools/modidump2ly.cpp
+++ tools/modidump2ly.cpp
@@ -10,9 +10,15 @@ class ModiDump2Ly {
   void SetOptions();
   void SetArgs(int argc, char **argv);
  private:
-  uint32_t debug() const { return debug_; }
+  uint32_t Debug() const { return debug_; }
+  void Help() const {
+    std::cout << desc_;
+  }
   po::options_description desc_; 
   po::variables_map vm_;
+  std::string dump_filename_;
+  std::string ly_filename_;
+  std::string debug_raw_;
   uint32_t debug_{0};
 };
 
@@ -24,14 +30,14 @@ void ModiDump2Ly::SetOptions() {
   desc_.add_options()
     ("help,h", "produce help message")
     ("input,i",
-      po::value<std::string>(),
+      po::value<std::string>(&dump_filename_)->required(),
       "Dump produced by modimidi")
     ("output,o",
-      po::value<std::string>(),
+      po::value<std::string>(&ly_filename_)->required(),
       "Output in Lilypond format")
     ("flat", po::bool_switch()->default_value(false),
       "Prefer flats♭ to default sharps♯")
-    ("debug", po::value<std::string>()->default_value("0"),
+    ("debug", po::value<std::string>(&debug_raw_)->default_value("0"),
       "Debug flags (hex ok)")
   ;
 }
@@ -42,8 +48,10 @@ void ModiDump2Ly::SetArgs(int argc, char **argv) {
       .run(),
     vm_);
   po::notify(vm_);
-  auto raw = vm_["debug"].as<std::string>();
-  debug_ = std::stoi(raw, nullptr, 0);
+  debug_ = std::stoi(debug_raw_, nullptr, 0);
+  if (vm_.count("help")) {
+    Help();
+  }
 }
 
 int main(int argc, char **argv) {
