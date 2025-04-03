a03bb20b91dca4a700431167755aad04c8ff350a
Yotam Medini <yotam.medini@gmail.com>
2025-04-03 18:23:06 +0300

    modidump2ly: program options cleaner required logic

--- tools/modidump2ly.cpp
+++ tools/modidump2ly.cpp
@@ -9,11 +9,13 @@ class ModiDump2Ly {
   ModiDump2Ly();
   void SetOptions();
   void SetArgs(int argc, char **argv);
+  int RC() const { return rc_; }
  private:
   uint32_t Debug() const { return debug_; }
-  void Help() const {
-    std::cout << desc_;
+  void Help(std::ostream &os) const {
+    os << desc_;
   }
+  int rc_{0};
   po::options_description desc_; 
   po::variables_map vm_;
   std::string dump_filename_;
@@ -30,11 +32,11 @@ void ModiDump2Ly::SetOptions() {
   desc_.add_options()
     ("help,h", "produce help message")
     ("input,i",
-      po::value<std::string>(&dump_filename_)->required(),
-      "Dump produced by modimidi")
+      po::value<std::string>(&dump_filename_),
+      "Dump produced by modimidi (required)")
     ("output,o",
-      po::value<std::string>(&ly_filename_)->required(),
-      "Output in Lilypond format")
+      po::value<std::string>(&ly_filename_),
+      "Output in Lilypond format (required)")
     ("flat", po::bool_switch()->default_value(false),
       "Prefer flats♭ to default sharps♯")
     ("debug", po::value<std::string>(&debug_raw_)->default_value("0"),
@@ -48,9 +50,14 @@ void ModiDump2Ly::SetArgs(int argc, char **argv) {
       .run(),
     vm_);
   po::notify(vm_);
+  if (dump_filename_.empty() || ly_filename_.empty()) {
+    rc_ = 1;
+    std::cerr << "Missing arguments (-i, -o)\n";
+    Help(std::cerr);
+  }
   debug_ = std::stoi(debug_raw_, nullptr, 0);
   if (vm_.count("help")) {
-    Help();
+    Help(std::cout);
   }
 }
 
