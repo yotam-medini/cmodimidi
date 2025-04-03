52f4c85a0af8e1f8b8e2cff317eaf42a818cc50a
Yotam Medini <yotam.medini@gmail.com>
2025-04-03 23:45:55 +0300

    modidump2ly += parse TimeSignature

--- tools/modidump2ly.cpp
+++ tools/modidump2ly.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <fstream>
+#include <regex>
 #include <unordered_map>
 #include <vector>
 #include <cstdint>
@@ -86,12 +88,15 @@ class ModiDump2Ly {
   ModiDump2Ly();
   void SetOptions();
   void SetArgs(int argc, char **argv);
+  int Run();
   int RC() const { return rc_; }
  private:
   uint32_t Debug() const { return debug_; }
   void Help(std::ostream &os) const {
     os << desc_;
   }
+  int Parse();
+  bool GetTrack(std::istream &ifs);
   int rc_{0};
   po::options_description desc_; 
   po::variables_map vm_;
@@ -140,9 +145,73 @@ void ModiDump2Ly::SetArgs(int argc, char **argv) {
   }
 }
 
+int ModiDump2Ly::Run() {
+  if (Debug() & 0x1) { std::cerr << "{Run\n"; }
+  Parse();
+  if (Debug() & 0x1) { std::cerr << "} end of Run\n"; }
+  return RC();
+}
+
+int ModiDump2Ly::Parse() {
+  if (Debug() & 0x1) { std::cerr << "{Parse\n"; }
+  std::ifstream ifs(dump_filename_);
+  if (ifs.fail()) {
+    rc_ = 1;
+    std::cerr << fmt::format("Failed to open {}\n", dump_filename_);
+  } else {
+    bool getting_tracks = true;
+    while (getting_tracks && (RC() == 0)) {
+      getting_tracks = GetTrack(ifs);
+    }
+  }
+  if (Debug() & 0x1) { std::cerr << "} end of Parse\n"; }
+  return RC();
+}
+
+bool ModiDump2Ly::GetTrack(std::istream &ifs) {
+  bool got = false;
+  for (bool skip = true; skip && not ifs.eof(); ) {
+    std::string line;
+    std::getline(ifs, line);
+    // skip = not line.starts_with("Track");
+    skip = (line.find("Track") != 0);
+  }
+  if (!ifs.eof()) {
+    got = true;
+    const std::regex base_regex(
+      "^.*AT=(\\d++),.* "
+      "TimeSignature\\(nn=(\\d+), dd=(\\d+), cc=(\\d+), bb=(\\d+)\\).*");
+    Track track;
+    std::string line;
+    // while (!(line.starts_with("}") || ifs.eof())) {
+    while (!((line.find("}") == 0) || ifs.eof())) {
+      std::getline(ifs, line);
+      std::smatch base_match;
+      if (std::regex_match(line, base_match, base_regex)) {
+        std::cout << "match_size=" << base_match.size() << '\n';
+        if (base_match.size() == 6) {
+          uint32_t abs_time = std::stoi(base_match[1].str());
+          uint8_t nn = std::stoi(base_match[2].str());
+          uint8_t dd = std::stoi(base_match[3].str());
+          uint8_t cc = std::stoi(base_match[4].str());
+          uint8_t bb = std::stoi(base_match[5].str());
+          TimeSignature ts(abs_time, nn, dd, cc, bb);
+          time_sigs_.push_back(std::move(ts));
+        }
+      }
+    }
+    tracks_.push_back(std::move(track));
+  }         
+  return got;
+}
+
 int main(int argc, char **argv) {
   std::cout << fmt::format("Hello {}\n", argv[0]);
   ModiDump2Ly modi_dump_2ly;
   modi_dump_2ly.SetArgs(argc, argv);
-  return 0;
+  int rc = modi_dump_2ly.RC();
+  if (rc == 0) {
+    rc = modi_dump_2ly.Run();
+  }
+  return rc;
 }
